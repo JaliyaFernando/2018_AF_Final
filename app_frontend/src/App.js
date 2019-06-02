65bcc2c832a69e8c8430836b11ab03e8a094d591
Jaliya <jaliyalaksirifernando@gmail.com>
2019-06-03 01:31:58 +0530

    Initial commit

---
+++ app_frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './navbar'
+import Main from './main';
+function App() {
+  return (
+    <div className="App">
+        <Nav/>
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
